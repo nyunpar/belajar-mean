a2702cfc1c9e7ebc03369974e8e2c885f1a608d7
Nirwan <ny.unpar@gmail.com>
2019-09-29 13:24:02 +0700

    siapkan dummy data di express

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('middleware pertama')
-    next();
-});
-app.use((req,res,next)=>{
-    res.send('hello from express');
+app.use('/api/posts',(req,res,next)=>{
+  const posts = [
+    {id:'fadfdasfdsa',
+     title:"Post dari server 1",
+    content: "Ini konten dari server 1" },
+    {id:'djfkasdjfkdf',
+     title:"Post dari server 2",
+    content: "Ini konten dari server 2" }
+  ]
+  res.status(200).json({
+    message:"post berhasil ditarik",
+    posts
+  });
 });
 module.exports = app;
